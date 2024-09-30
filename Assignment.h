9898e95b90386aafb6ba03284b7d44548d8b4fc2
Aghas04 <ghasemialex83@gmail.com>
2024-09-30 12:19:31 -0500

    Delete Assignment.h

--- Assignment.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-template <typename Item_Type>
-struct SNode {
-  //data
-  Item_Type data;
-  SNode* next;
-
-  SNode(const Item_Type& data_item, SNode* next_ptr = NULL) : data(data_item), next(next_ptr) {}
-};
